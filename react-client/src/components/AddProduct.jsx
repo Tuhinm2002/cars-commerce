14b45f006193a77ad792edb042a34b296812939e
unknown <tuhinm2002@gmail.com>
2024-09-03 23:08:11 +0530

    page updated

--- react-client/src/components/AddProduct.jsx
+++ react-client/src/components/AddProduct.jsx
@@ -5,38 +5,31 @@ import React from "react";
 export default function AddProduct(){
 
     return(
-    <form class="row g-3">
+    <form class="row g-3 mt-4 ml-2">
   <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
+    <label for="inputEmail4" class="form-label">Vehicle Name</label>
     <input type="email" class="form-control" id="inputEmail4" />
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
+    <label for="inputPassword4" class="form-label">Vehicle Category</label>
     <input type="password" class="form-control" id="inputPassword4" />
   </div>
   <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
+    <label for="inputAddress" class="form-label">Vehicle model</label>
     <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
   </div>
   <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
+    <label for="inputAddress2" class="form-label">Vehicle brand</label>
     <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
   </div>
   <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
+    <label for="inputCity" class="form-label">Date</label>
     <input type="text" class="form-control" id="inputCity" />
   </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip" />
-  </div>
+  <div class="mb-3">
+  <label for="formFileMultiple" class="form-label">Vehicle image</label>
+  <input class="form-control" type="file" id="formFileMultiple" multiple />
+</div>
   <div class="col-12">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck" />
