a91fb35b3bd65b10389168d34351ef29829ba4cc
unknown <tuhinm2002@gmail.com>
2024-09-05 20:11:54 +0530

    add section improved

--- react-client/src/components/AddProduct.jsx
+++ react-client/src/components/AddProduct.jsx
@@ -8,36 +8,57 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useState } from 'react';
 
 function GridComplexExample() {
+  const [dateValue,setDateValue] = useState(null)
+  function submitValue(){
+    
+    try{
+    const brand = document.getElementById('formGridBrand')
+    const model = document.getElementById('formGridModel')
+    const category = document.getElementById('formGridCategory')
+    const name = document.getElementById('formGridFullName')
+    const available = document.getElementById('formGridCheckbox')
+    console.log(dateValue)
+
+    }
+    catch{
+      console.log("null")
+    }
+
+  }
+
+  // submitValue()
+
   return (
     <Form className='w-25 mt-4' style={{margin:"auto"}}>
       <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Group as={Col} controlId="formGridBrand">
           <Form.Label>Vehicle Brand</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="text" placeholder="Ex: BMW" />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridPassword">
+        <Form.Group as={Col} controlId="formGridModel">
           <Form.Label>Vehicle Model</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="text" placeholder="Ex: xf,xt, etc.." />
         </Form.Group>
       </Row>
 
-      <Form.Group className="mb-3 ml-4 " controlId="formGridAddress1">
+      <Form.Group className="mb-3 ml-4 " controlId="formGridCategory">
         <Form.Label>Vehicle Category</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+        <Form.Control placeholder="Ex: Cars,Trucks,Trains ..." />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
+      <Form.Group className="mb-3" controlId="formGridFullName">
         <Form.Label>Vehicle Full Name</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
+        <Form.Control placeholder="Ex: BMW X3.." />
       </Form.Group>
 
       <Row className="mb-3">
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker']}>
-        <DatePicker label="Basic date picker" />
+      <DemoContainer components={['DatePicker']} >
+        <DatePicker value={dateValue} onChange={(newValue) => setDateValue(newValue)} label="Basic date picker"/>
       </DemoContainer>
     </LocalizationProvider>
 
@@ -51,7 +72,7 @@ function GridComplexExample() {
         <Form.Check type="checkbox" label="Is Available" />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={submitValue()} >
         Submit
       </Button>
     </Form>
