1823bc1a4600b56a19b81072f0f5fd3feb38852e
unknown <tuhinm2002@gmail.com>
2024-09-16 19:12:07 +0530

    ui improved

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from './images/audi_a8.jpg';
+import img2 from './images/red_coup_st.jpg';
+import img3 from './images/lamborghini_aventidor.jpg';
 
-export default function Home(){
-    return(
-        <>
-        Home
-        </>
-    )
+function Home() {
+  return (
+    <Carousel >
+      <Carousel.Item style={{backgroundColor:"#979b3c",maxHeight:250}}>
+        <img src={img1} alt="" width={400} />
+        <Carousel.Caption>
+          <h3>Buy Cars</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{backgroundColor:"#1b3f66",maxHeight:250}}>
+      <img src={img2} alt="" width={400} />
+        <Carousel.Caption>
+          <h3>Sell Cars</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item style={{backgroundColor:"#9c321c",maxHeight:250}}>
+      <img src={img3} alt="" width={400}/>
+        <Carousel.Caption>
+          <h3>Get the best value</h3>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
+
+export default Home;
